f1c912f35709c25baea79a9d984ce9aef6a22752
Kyle Fuller <inbox@kylefuller.co.uk>
2014-12-13 23:47:16 +0000

    Implement register device endpoint
    Closes #2

--- palaverapi/tests/test_views.py
+++ palaverapi/tests/test_views.py
@@ -2,12 +2,22 @@ import unittest
 from rivr.tests import TestClient
 
 from palaverapi.views import router
+from palaverapi.models import Device, Token
 
 
-class StatusTests(unittest.TestCase):
+class ViewTests(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(router)
 
     def test_status(self):
         assert self.client.get('/').status_code is 204
+
+    def test_register(self):
+        response = self.client.post('/1/devices', {'device_token': 'test_token'})
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        self.assertEqual(response.content, '{"device_token": "test_token", "push_token": "ec1752bd70320e4763f7165d73e2636cca9e25cf"}')
+
+        device = Device.get(apns_token='test_token')
+        tokens = Token.select().where(Token.device == device)
+        self.assertEqual(tokens.count(), 2)
 
--- palaverapi/views.py
+++ palaverapi/views.py
@@ -1,5 +1,10 @@
+import hashlib
+
 from rivr.router import Router
-from rivr.http import Response
+from rivr.http import Response, RESTResponse
+from rivr.views import RESTView
+
+from palaverapi.models import database, Device, Token
 
 
 router = Router()
@@ -8,4 +13,22 @@ router = Router()
 def status(request):
     return Response(status=204)
 
+
+class RegisterView(RESTView):
+    def post(self, request):
+        apns_token = request.POST['device_token']
+        push_token = hashlib.sha1(hashlib.sha1(apns_token + apns_token).hexdigest()).hexdigest()
+
+        with database.transaction():
+            device = Device.create(apns_token=apns_token)
+            token = Token.create(device=device, token=apns_token, scope=Token.ALL_SCOPE)
+            push = Token.create(device=device, token=push_token, scope=Token.PUSH_SCOPE)
+
+        return RESTResponse(request, {
+            'device_token': apns_token,
+            'push_token': push_token,
+        }, status=201)
+
+
+router.register(r'^1/devices$', RegisterView.as_view())
 
