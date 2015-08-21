f5b2b9b71df7e5649e5d8a18e7a0060ec6273b76
Kyle Fuller <kyle@fuller.li>
2015-08-20 17:01:49 -0700

    Stub the redis queue during tests

--- palaverapi/tests/test_views.py
+++ palaverapi/tests/test_views.py
@@ -1,7 +1,7 @@
 import unittest
 from rivr.tests import TestClient
 
-from palaverapi.views import router
+from palaverapi.views import router, queue
 from palaverapi.models import Device, Token
 
 
@@ -46,10 +46,17 @@ class ViewTests(unittest.TestCase):
         self.assertEqual(response.status_code, 401)
 
     def test_push(self):
+        enqueued = []
+
+        def enqueue(*args):
+            enqueued.append(args)
+        queue.enqueue = enqueue
+
         device = Device.create(apns_token='ec1752bd70320e4763f7165d73e2636cca9e25cf')
         token = Token.create(device=device, token='valid', scope=Token.ALL_SCOPE)
 
         headers = { 'AUTHORIZATION': 'token valid' }
         response = self.client.post('/1/push', {}, headers)
         self.assertEqual(response.status_code, 202)
+        self.assertEqual(len(enqueued), 1)
 
