e9624982732c03e68525c2894c5f95d2cad2487c
Kyle Fuller <kyle@fuller.li>
2016-05-27 13:50:28 -0700

    Add bugsnag to log errors

--- palaverapi/__init__.py
+++ palaverapi/__init__.py
@@ -1,31 +1,17 @@
 import os
 import logging
-from logging.handlers import SMTPHandler
 
 from rivr.wsgi import WSGIHandler
+
 from palaverapi.views import app
 
 logger = logging.getLogger('rivr.request')
 
-if os.environ.get('SENDGRID_USERNAME'):
-    mail_handler = SMTPHandler('smtp.sendgrid.net',
-                               'support@palaverapp.com',
-                               ['support@palaverapp.com'],
-                               '[API Error] Heroku',
-                               credentials=(os.environ['SENDGRID_USERNAME'],
-                                            os.environ['SENDGRID_PASSWORD']))
-    mail_handler.setLevel(logging.ERROR)
-    logger.addHandler(mail_handler)
-    mail_handler.setFormatter(logging.Formatter("""\
-Message type:       %(levelname)s
-Location:           %(pathname)s:%(lineno)d
-Module:             %(module)s
-Function:           %(funcName)s
-Time:               %(asctime)s
-
-Message:
-
-%(message)s"""))
+if os.environ.get('BUGSNAG_API_KEY'):
+    from bugsnag.handlers import BugsnagHandler
+    handler = BugsnagHandler()
+    handler.setLevel(logging.ERROR)
+    logger.addHandler(handler)
 else:
     console = logging.StreamHandler()
     console.setLevel(logging.ERROR)
@@ -33,4 +19,3 @@ else:
 
 
 wsgi = WSGIHandler(app)
-
