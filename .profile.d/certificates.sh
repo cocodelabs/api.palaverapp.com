e5a77075f683a4665151d1d10ffad79c6e0bbaf8
Dennis Collaris <d.collaris@me.com>
2019-01-04 23:15:48 +0100

    refactor: switch to HTTP based APNS API

--- .profile.d/certificates.sh
+++ .profile.d/certificates.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir -p certificates
+
 echo "$APNS_PRIVATE_KEY" > certificates/private.pem
 echo "$APNS_PUBLIC_KEY" > certificates/public.pem
 
+echo "$APNS_PRIVATE_KEY" > certificates/production.pem
+echo "$APNS_PUBLIC_KEY" >> certificates/production.pem
