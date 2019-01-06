dfe4a753fcf163daa163f73dfc00d451a3c3dd78
Kyle Fuller <kyle@fuller.li>
2019-01-06 00:10:16 +0000

    feat: add authorisations API

--- palaverapi/tests/test_views.py
+++ palaverapi/tests/test_views.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from rivr.tests import TestClient
 
 from palaverapi.views import router, queue
@@ -66,3 +67,113 @@ class ViewTests(unittest.TestCase):
 
         token.delete_instance()
         device.delete_instance()
+
+
+class AuthorisationListViewTests(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(router)
+
+        self.device = Device.create(apns_token='ec1752bd70320e4763f7165d73e2636cca9e25cf')
+        self.token = Token.create(device=self.device, token='e4763f7165d73e2636cca9e', scope=Token.ALL_SCOPE)
+
+    def tearDown(self):
+        self.token.delete_instance()
+        self.device.delete_instance()
+
+    def test_list(self):
+        headers = { 'AUTHORIZATION': 'token e4763f7165d73e2636cca9e' }
+        response = self.client.get('/authorisations', {}, headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), [
+            {
+                'url': '/authorisations/636cca9e',
+                'scopes': ['all'],
+                'token_last_eight': '636cca9e'
+            }
+        ])
+
+    def test_create(self):
+        headers = { 'AUTHORIZATION': 'token e4763f7165d73e2636cca9e' }
+        response = self.client.post('/authorisations', {}, headers)
+
+        self.assertEqual(response.status_code, 201)
+
+        content = json.loads(response.content)
+        self.assertTrue('url' in content)
+        self.assertTrue('token' in content)
+        self.assertTrue('token_last_eight' in content)
+        #self.assertEqual(len('token_last_eight'), 8)
+        self.assertTrue('scopes' in content)
+        self.assertEqual(content['scopes'], ['all'])
+
+        token = Token.select().where(Token.token == content['token']).get()
+        token.delete_instance()
+
+    def test_create_with_scope(self):
+        headers = { 'AUTHORIZATION': 'token e4763f7165d73e2636cca9e' }
+        response = self.client.post('/authorisations', {'scopes': ['push']}, headers)
+
+        self.assertEqual(response.status_code, 201)
+
+        content = json.loads(response.content)
+        self.assertTrue('url' in content)
+        self.assertTrue('token' in content)
+        self.assertTrue('token_last_eight' in content)
+        #self.assertEqual(len('token_last_eight'), 8)
+        self.assertTrue('scopes' in content)
+        #self.assertEqual(content['scopes'], ['push'])
+
+        token = Token.select().where(Token.token == content['token']).get()
+        token.delete_instance()
+
+    def test_create_with_token(self):
+        headers = { 'AUTHORIZATION': 'token e4763f7165d73e2636cca9e' }
+        response = self.client.post('/authorisations', {'token': '4876f9ca0d91362fae6cd4f9cde5d0044295682e'}, headers)
+
+        self.assertEqual(response.status_code, 201)
+
+        content = json.loads(response.content)
+        self.assertTrue('url' in content)
+        self.assertTrue('token' in content)
+        self.assertTrue('token_last_eight' in content)
+        #self.assertEqual(len('token_last_eight'), 8)
+        self.assertTrue('scopes' in content)
+        self.assertEqual(content['token'], '4876f9ca0d91362fae6cd4f9cde5d0044295682e')
+
+        token = Token.select().where(Token.token == content['token']).get()
+        token.delete_instance()
+
+
+class AuthorisationDetailViewTests(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(router)
+
+        self.device = Device.create(apns_token='ec1752bd70320e4763f7165d73e2636cca9e25cf')
+        self.token = Token.create(device=self.device, token='e4763f7165d73e2636cca9e', scope=Token.ALL_SCOPE)
+
+    def tearDown(self):
+        if self.token:
+            self.token.delete_instance()
+
+        self.device.delete_instance()
+
+    def test_get(self):
+        headers = { 'AUTHORIZATION': 'token e4763f7165d73e2636cca9e' }
+        response = self.client.get('/authorisations/636cca9e', {}, headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), {
+            'url': '/authorisations/636cca9e',
+            'scopes': ['all'],
+            'token_last_eight': '636cca9e'
+        })
+
+    def test_delete(self):
+        headers = { 'AUTHORIZATION': 'token e4763f7165d73e2636cca9e' }
+        response = self.client.delete('/authorisations/636cca9e', {}, headers)
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Token.select().count(), 0)
+
+        self.token = None
