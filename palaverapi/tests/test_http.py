f8fbba50f58950881e61b4eb94f3719605f5243d
Kyle Fuller <kyle@fuller.li>
2019-07-23 14:30:55 +0100

    fix: return problem+json for all errors

---
+++ palaverapi/tests/test_http.py
@@ -0,0 +1,23 @@
+import unittest
+import json
+
+from rivr.tests import TestClient
+
+from palaverapi.views import app
+
+
+class HTTPTests(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(app)
+
+    def test_404(self):
+        response = self.client.get('/404')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.headers['Content-Type'], 'application/problem+json')
+        self.assertEqual(response.content, '{"title": "Resource Not Found"}')
+
+    def test_500(self):
+        response = self.client.get('/500')
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.headers['Content-Type'], 'application/problem+json')
+        self.assertEqual(response.content, '{"title": "Internal Server Error"}')
