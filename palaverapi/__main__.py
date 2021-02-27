1caff31f413f07d7f0e0fd3c14efaab77eeb6698
Kyle Fuller <kyle@fuller.li>
2021-02-27 16:44:11 +0000

    refactor: appease mypy

--- palaverapi/__main__.py
+++ palaverapi/__main__.py
@@ -1,6 +1,6 @@
 from rivr import serve
 
-from palaverapi.views import app
+from palaverapi import app
 
 if __name__ == '__main__':
     serve(app)
--- palaverapi/decorators.py
+++ palaverapi/decorators.py
@@ -12,23 +12,23 @@ def requires_body(func: Callable[..., Response]):
     @wraps(func)
     def wrapper(self, request: Request) -> Response:
         if request.content_type:
-            content = request.body.read()
+            body = request.body.read()
             content_type = request.content_type.split(';')[0]
 
             try:
                 if content_type == 'application/json':
-                    content = content.decode('utf-8')
-                    body = json.loads(content)
+                    content = body.decode('utf-8')
+                    payload = json.loads(content)
                 elif content_type == 'application/x-www-form-urlencoded':
-                    content = content.decode('utf-8')
+                    content = body.decode('utf-8')
                     data = parse_qsl(content, True)
-                    body = dict((k, v) for k, v in data)
+                    payload = dict((k, v) for k, v in data)
                 else:
                     return ProblemResponse(415, 'Unsupported Media Type')
             except (UnicodeDecodeError, ValueError):
                 return ProblemResponse(400, 'Invalid request body')
 
-            return func(self, request, body)
+            return func(self, request, payload)
 
         return func(self, request, {})
 
